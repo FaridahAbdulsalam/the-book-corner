e0ae1b4b96d5ece2a7d32d45ae52532f0f77c071
Faridah Salami <f.salami@outlook.com>
2024-06-02 12:01:04 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+const App = () => {
+  return (
+    <>
+    <header>
+      <h1>
+        The Book Corner
+      </h1>
+    </header>
+    <div>
+      <input type="text" placeholder="Title, author, id, gen..." />
+    </div>
+    <section>
+      <h2>Current Reads</h2>
+    </section>
+    <section>
+      <h2>
+        Top Picks
+      </h2>
+    </section>
+    <footer>
+      <h3>'footer icons'</h3>
+    </footer>
+    </>
+
+  )
+}
+
+export default App
